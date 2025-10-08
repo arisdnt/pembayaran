a2f33664cd77696c8d4088bf52ad531ee91bb6e7
arisdnt <arisdianto@gmail.com>
2025-10-08 18:56:07 +0700

    Update 2025-10-08 18:56:07

--- src/pages/JenisPembayaran/utils/badgeHelper.js
+++
@@ -1,8 +0,0 @@
-export function getTipeBadgeColor(tipe) {
-  switch (tipe) {
-    case 'bulanan': return 'blue'
-    case 'tahunan': return 'purple'
-    case 'sekali': return 'green'
-    default: return 'gray'
-  }
-}
